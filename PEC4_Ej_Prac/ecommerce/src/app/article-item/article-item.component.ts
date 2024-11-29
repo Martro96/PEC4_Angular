bf4c4b1c2a308f4d185088a95dd8204520af67ee
Martro96 <martarodrigo.55@gmail.com>
2024-11-29 22:13:58 +0100

    Creo componente e interface

---
+++ PEC4_Ej_Prac/ecommerce/src/app/article-item/article-item.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Article } from './article-item.interface';
+
+@Component({
+  selector: 'app-article-item',
+  imports: [],
+  templateUrl: './article-item.component.html',
+  styleUrl: './article-item.component.css'
+})
+export class ArticleItemComponent {
+ article: Article = {
+  name: 'Zanahoria',
+  imageUrl: 'https://soycomocomo.es/media/2019/03/zanahorias.jpg',
+  price: 1,
+  isOnSale: true,
+  quantityInCart: 5
+ }
+}
